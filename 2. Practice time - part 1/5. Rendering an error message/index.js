6bcd1b2b98842f686d4e28b90942e54e28e49660
Saksham Gupta <62757934+G-Saksham@users.noreply.github.com>
2022-04-01 18:13:08 +0530

    error alert

--- 2. Practice time - part 1/5. Rendering an error message/index.js
+++ 2. Practice time - part 1/5. Rendering an error message/index.js
@@ -1,5 +1,11 @@
 // When the user clicks the purchase button, render out
 // "Something went wrong, please try again" in the paragraph
 // that has the id="error".
+const errorEl = document.getElementById(error)
+
+function errorAlert() {
+  erroeEL.textContent = "Something went wrong, please try again"
+}
+
 
 
